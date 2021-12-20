50f9900a7264f1265bc133fe9a362c66dd7765c4
Melina <mel.lera23@gmail.com>
2021-12-19 22:07:05 -0300

    agrego navBar

--- muebles3.30.lera/src/App.js
+++ muebles3.30.lera/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import{ BrowserRouter as Router, Switch,Route } from 'react-router-dom'
+import NavBar from './components/Navegacion/NavBar';
+import inicio from './components/paginas/inicio';
+import productos from './components/paginas/productos';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+       <Router>
+         <NavBar/>
+        </Router> 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
