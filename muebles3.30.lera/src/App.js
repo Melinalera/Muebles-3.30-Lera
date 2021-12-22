066f7461f9bfeb28068ad115649f7281cd21f217
Melina <mel.lera23@gmail.com>
2021-12-21 21:00:42 -0300

    modifico navbr

--- muebles3.30.lera/src/App.js
+++ muebles3.30.lera/src/App.js
@@ -1,29 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import{ BrowserRouter as Router, Switch,Route } from 'react-router-dom'
-import NavBar from './components/Navegacion/NavBar';
-import inicio from './components/paginas/inicio';
-import productos from './components/paginas/productos';
+import React from 'react'
+import NavBarBS from  './components/Navegacion/BarraDeNavegacion/NavBarBS'
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-       <Router>
-         <NavBar/>
-        </Router> 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+         <NavBarBS />
+         
+    
     </div>
   );
 }
